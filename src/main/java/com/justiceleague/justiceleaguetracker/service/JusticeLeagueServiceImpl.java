99e2d943dfdd0f2599405db6bbd6be3da3986e70
Peter <jiayong12381@gmail.com>
2019-12-15 18:01:32 +1100

    Update JusticeLeagueServiceImpl.java

--- src/main/java/com/justiceleague/justiceleaguetracker/service/JusticeLeagueServiceImpl.java
+++ src/main/java/com/justiceleague/justiceleaguetracker/service/JusticeLeagueServiceImpl.java
@@ -31,7 +31,7 @@ public class JusticeLeagueServiceImpl implements JusticeLeagueService {
                 "Tapping into the speed force", "Time travel"), "Central city");
         SuperHero batman = new SuperHero("Bruce Wayne", Arrays.asList("Super rich", "Cool Gadgets"),
                 "Gotham");
-        SuperHero batman = new SuperHero("Jack Chen", Arrays.asList("Gongfu King", "Hot Star"),
+        SuperHero Jack = new SuperHero("Jack Chen", Arrays.asList("Gongfu King", "Hot Star"),
                 "Donts");
         return Arrays.asList(superMan, flash, batman);
     }
